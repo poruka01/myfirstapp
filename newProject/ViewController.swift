8e620b3946878c1e5ea0e37bd0d92a8b9df5de30
pokeruka <haruka@highland-mac-book-air.local>
2018-03-29 14:03:11 +0900

    new インスタンス作成

--- newProject/ViewController.swift
+++ newProject/ViewController.swift
@@ -10,10 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var new = ""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("New World")
+        print(new)
     }
 
     override func didReceiveMemoryWarning() {
