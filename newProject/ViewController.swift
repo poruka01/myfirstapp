647d2c40163b87f9f3ed28c355f5e78d8a6d102a
pokeruka <haruka@highland-mac-book-air.local>
2018-03-29 21:33:10 +0900

    枝分れしたい

--- newProject/ViewController.swift
+++ newProject/ViewController.swift
@@ -16,6 +16,8 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         print(new)
+        
+        //newoneなっていますでしょうか
     }
 
     override func didReceiveMemoryWarning() {
